6212fcd4fee2199fa4768bc26697ac590ef32d9f
GesaLoof <gesaloof@gmail.com>
2025-08-05 15:42:05 +0200

    removed old remant unet.py

--- src/em_nuclear_segmentation/models/unet.py
+++ src/em_nuclear_segmentation/models/unet.py
@@ -2,17 +2,21 @@ import torch
 import torch.nn as nn
 
 class UNet(nn.Module):
-    def __init__(self, in_channels=1, out_channels=1):
+    def __init__(self, in_channels=1, out_channels=1, dropout_prob=0.0):
         super(UNet, self).__init__()
 
-        def conv_block(in_c, out_c):
-            return nn.Sequential(
+        def conv_block(in_c, out_c, dropout=0.0):
+            layers = [
                 nn.Conv2d(in_c, out_c, kernel_size=3, padding=1),
                 nn.ReLU(inplace=True),
                 nn.Conv2d(out_c, out_c, kernel_size=3, padding=1),
                 nn.ReLU(inplace=True)
-            )
+            ]
+            if dropout > 0.0:
+                layers.append(nn.Dropout(p=dropout))
+            return nn.Sequential(*layers)
 
+        # Encoder (no dropout here by default)
         self.encoder = nn.ModuleList([
             conv_block(in_channels, 64),
             conv_block(64, 128),
@@ -22,7 +26,8 @@ class UNet(nn.Module):
 
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.bottleneck = conv_block(512, 1024)
+        # Bottleneck (dropout optional)
+        self.bottleneck = conv_block(512, 1024, dropout=dropout_prob)
 
         self.upconvs = nn.ModuleList([
             nn.ConvTranspose2d(1024, 512, 2, 2),
@@ -31,11 +36,12 @@ class UNet(nn.Module):
             nn.ConvTranspose2d(128, 64, 2, 2),
         ])
 
+        # Decoder (apply dropout here)
         self.decoder = nn.ModuleList([
-            conv_block(1024, 512),
-            conv_block(512, 256),
-            conv_block(256, 128),
-            conv_block(128, 64),
+            conv_block(1024, 512, dropout=dropout_prob),
+            conv_block(512, 256, dropout=dropout_prob),
+            conv_block(256, 128, dropout=dropout_prob),
+            conv_block(128, 64, dropout=dropout_prob),
         ])
 
         self.output = nn.Conv2d(64, out_channels, kernel_size=1)
